edf6f03269a4b950ec26c77c9c94b3236a90483a
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-10-20 15:38:02 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -2,35 +2,38 @@
 #include <assert.h>
 #include <string.h>
 
-// Function to return a formatted string for testing purposes
+// Function to get a formatted string for a color map entry
 const char* getColorMapEntry(int majorIndex, int minorIndex) {
     const char* majorColor[5] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[5] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    
     static char colorPair[50];
-    sprintf(colorPair, "%d | %s | %s", majorIndex * 5 + minorIndex, majorColor[majorIndex], minorColor[minorIndex]);
+    sprintf(colorPair, "%2d | %-6s | %-6s", majorIndex * 5 + minorIndex, majorColor[majorIndex], minorColor[minorIndex]);
     return colorPair;
 }
 
-int printColorMap() {
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) {
-        for(j = 0; j < 5; j++) {
-            printf("%s\n", getColorMapEntry(i, j));
+// Function to print the color map using the getColorMapEntry function
+void printColorMap() {
+    int majorIndex, minorIndex;
+    for ( majorIndex = 0; majorIndex < 5; majorIndex++) {
+        for (minorIndex = 0; minorIndex < 5; minorIndex++) {
+            printf("%s\n", getColorMapEntry(majorIndex, minorIndex));
         }
     }
-    return i * j;
+}
+
+// Test function to check if the color map entries are correctly aligned and formatted
+void testColorMap() {
+    assert(strcmp(getColorMapEntry(0, 0), " 0 | White  | Blue  ") == 0);
+    assert(strcmp(getColorMapEntry(1, 1), " 6 | Red    | Orange") == 0);
+    assert(strcmp(getColorMapEntry(2, 2), "12 | Black  | Green ") == 0);
+    assert(strcmp(getColorMapEntry(3, 3), "18 | Yellow | Brown ") == 0);
+    assert(strcmp(getColorMapEntry(4, 4), "24 | Violet | Slate ") == 0);
+    assert(strcmp(getColorMapEntry(1, 2), "7 | Red | Green") != 0);
 }
 
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
-    assert(strcmp(getColorMapEntry(0, 0), "0 | White | Blue") == 0);
-    assert(strcmp(getColorMapEntry(1, 1), "6 | Red | Orange") == 0);
-    assert(strcmp(getColorMapEntry(2, 2), "12 | Black | Green") == 0);
-    assert(strcmp(getColorMapEntry(3, 3), "18 | Yellow | Brown") == 0);
-    assert(strcmp(getColorMapEntry(4, 4), "24 | Violet | Slate") == 0);
-    assert(strcmp(getColorMapEntry(1, 2), "7 | Red | Green") == 0);
+    testColorMap();
+    printColorMap();
     printf("All is well (maybe!)\n");
     return 0;
 }
