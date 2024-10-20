c0b1ca594a2c2bca919953fdf682b7bb64f6679d
vacuuum <47915224+vacuuum@users.noreply.github.com>
2024-10-20 15:08:14 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <assert.h>
 
-char size(int cms) {
+char size(int cms) 
+{
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms <= 38) 
+    {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } 
+    else if(cms > 38 && cms <= 42)
+    {
         sizeName = 'M';
-    } else if(cms > 42) {
+    }
+    else if(cms > 42) 
+    {
         sizeName = 'L';
+    }
+    else
+    {
+        /* do nothing */
     }
     return sizeName;
 }
 
 int main() {
     assert(size(37) == 'S');
-    assert(size(40) == 'M');
+    assert(size(38) == 'S'); // Test the boundary condition
+    assert(size(39) == 'M');
+    assert(size(42) == 'M'); // Test the boundary condition
     assert(size(43) == 'L');
     printf("All is well (maybe!)\n");
     return 0;
